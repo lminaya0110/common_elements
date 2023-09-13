c2da02beb30986264f16c0bb3aeea669c4bd7b38
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-13 10:24:20 -0400

    added instructional comment

--- lab.py
+++ lab.py
@@ -1,5 +1,10 @@
 import random
 
+'''
+This lab is solving the following problem:
+    Code a solution that takes two lists and compares all elements. All common elements should be added to a new list and printed out in terminal.
+'''
+
 num_in_list = int(input("Enter a number between 10 and 20 ===> "))
 
 list_1 = [random.randint(1, num_in_list) for i in range(num_in_list)]
