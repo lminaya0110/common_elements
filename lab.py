690a2d4b8618b50e982237a2568f3ac8733adf68
lminaya0110 <100318087+lminaya0110@users.noreply.github.com>
2023-09-13 10:21:25 -0400

    completed morning lab

---
+++ lab.py
@@ -0,0 +1,20 @@
+import random
+
+num_in_list = int(input("Enter a number between 10 and 20 ===> "))
+
+list_1 = [random.randint(1, num_in_list) for i in range(num_in_list)]
+
+list_2 = [random.randint(1, num_in_list) for i in range(num_in_list)]
+
+print(f'List 1 : {list_1}\nList 2: {list_2}')
+
+common_elems = []
+
+for element in list_1:
+    if element in list_2:
+        common_elems.append(element)
+
+
+list_1.sort()
+list_2.sort()
+print(f'List 1 : {list_1}\nList 2 : {list_2}\n\nCommon Elements : {common_elems}')
